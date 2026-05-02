b18a66583315de9019a91569f2969e84829cbcd7
LordPercivalXII <officialtwelfthdoctor1@hotmail.com>
2026-05-02 22:05:55 +0800

    Add Extended and Ultimate editions of deck

--- LP_XII_DeckSkin.lua
+++ LP_XII_DeckSkin.lua
@@ -3,6 +3,15 @@
 -- Base Definitions
 
 local atlas_base = "lp_n_co"
+local suits = {"Hearts", "Clubs", "Diamonds", "Spades"}
+local ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
+local descriptions = {
+	"Lord Percival XII & Co. Deck Skin", 
+	"SPARE",
+	"Lord Percival XII & Co.",
+	"Lord Percival XII & Co. - Extended",
+	"Lord Percival XII & Co. - Ultimate"
+}
 
 -- Atlas definitions
 
@@ -12,6 +21,9 @@ local mod_icon = SMODS.Atlas {
 	px = 32,
 	py = 32,
 }
+
+-- Collab Based Definitions
+-- Note: King, Queen and Jack cards only
 
 local atlas_hearts_hc = SMODS.Atlas {
 	key = atlas_base .. "_hearts_hc",
@@ -69,19 +81,53 @@ local atlas_clubs_lc = SMODS.Atlas {
 	py = 95,
 }
 
+-- Suit Based Definitions
+-- Note: King, Queen, Jack and Ace cards only
+
+local atlas_suit_hc = SMODS.Atlas {
+	key = atlas_base .. "_deck_hc",
+	path = atlas_base .. "_deck_hc.png",
+	px = 71,
+	py = 95,
+}
+
+local atlas_suit_lc = SMODS.Atlas {
+	key = atlas_base .. "_deck_lc",
+	path = atlas_base .. "_deck_lc.png",
+	px = 71,
+	py = 95,
+}
+
+-- Full Suit Based Definitions
+-- Note: All cards included
+
+local atlas_full_hc = SMODS.Atlas {
+	key = atlas_base .. "_deck_custom_hc",
+	path = atlas_base .. "_deck_custom_hc.png",
+	px = 71,
+	py = 95,
+}
+
+local atlas_full_lc = SMODS.Atlas {
+	key = atlas_base .. "_deck_custom_lc",
+	path = atlas_base .. "_deck_custom_lc.png",
+	px = 71,
+	py = 95,
+}
+
 -- Crediting Functions Base
 
 -- Has Deck Skin Card UI
 -- Note: The function assumes that the implemented designs are made by one artist.
 has_ds_card_ui_base = function(card, deckskin, palette)
-	if card.base.value == "King" or card.base.value == "Queen" or card.base.value == "Jack" then
+	if card.base.value == "King" or card.base.value == "Queen" or card.base.value == "Jack" or card.base.value == "Ace" then
 		return true
 	end
 	return false
 end
 
 generate_ds_card_ui_base = function(card, deckskin, palette, info_queue, desc_nodes, specific_vars, full_UI_table)
-    if card.base.value == "King" or card.base.value == "Queen" or card.base.value == "Jack" then
+    if card.base.value == "King" or card.base.value == "Queen" or card.base.value == "Jack" or card.base.value == "Ace" then
         localize{type = 'other', key = 'artist', nodes = desc_nodes, vars = {}} 
         localize{type = 'other', key = 'artist_credit', nodes = desc_nodes, vars = { "LordPercivalXII" }}
     end
@@ -89,10 +135,13 @@ end
 
 -- Deck Skin definition
 
+-- Collab Based Definitions
+-- Note: King, Queen and Jack cards only
+
 SMODS.DeckSkin {
 	key = atlas_base .. "_hearts",
 	suit = "Hearts",
-	loc_txt = "Lord Percival XII & Co.",
+	loc_txt = descriptions[3],
 	palettes = {
 		{
 			key = 'lc',
@@ -103,7 +152,7 @@ SMODS.DeckSkin {
 		},
 		{
 			key = 'hc',
-			ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",},
+			ranks = {'Jack', 'Queen', "King",},
 			display_ranks = {"King", "Queen", "Jack"},
 			atlas = atlas_hearts_hc.key,
 			pos_style = 'collab',
@@ -116,7 +165,7 @@ SMODS.DeckSkin {
 SMODS.DeckSkin {
 	key = atlas_base .. "_spades",
 	suit = "Spades",
-	loc_txt = "Lord Percival XII & Co.",
+	loc_txt = descriptions[3],
 	palettes = {
 		{
 			key = 'lc',
@@ -127,7 +176,7 @@ SMODS.DeckSkin {
 		},
 		{
 			key = 'hc',
-			ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",},
+			ranks = {'Jack', 'Queen', "King",},
 			display_ranks = {"King", "Queen", "Jack"},
 			atlas = atlas_spades_hc.key,
 			pos_style = 'collab',
@@ -140,7 +189,7 @@ SMODS.DeckSkin {
 SMODS.DeckSkin {
 	key = atlas_base .. "_diamonds",
 	suit = "Diamonds",
-	loc_txt = "Lord Percival XII & Co.",
+	loc_txt = descriptions[3],
 	palettes = {
 		{
 			key = 'lc',
@@ -151,7 +200,7 @@ SMODS.DeckSkin {
 		},
 		{
 			key = 'hc',
-			ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",},
+			ranks = {'Jack', 'Queen', "King",},
 			display_ranks = {"King", "Queen", "Jack"},
 			atlas = atlas_diamonds_hc.key,
 			pos_style = 'collab',
@@ -164,7 +213,7 @@ SMODS.DeckSkin {
 SMODS.DeckSkin {
 	key = atlas_base .. "_clubs",
 	suit = "Clubs",
-	loc_txt = "Lord Percival XII & Co.",
+	loc_txt = descriptions[3],
 	palettes = {
 		{
 			key = 'lc',
@@ -175,7 +224,7 @@ SMODS.DeckSkin {
 		},
 		{
 			key = 'hc',
-			ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",},
+			ranks = {'Jack', 'Queen', "King",},
 			display_ranks = {"King", "Queen", "Jack"},
 			atlas = atlas_clubs_hc.key,
 			pos_style = 'collab',
@@ -184,3 +233,61 @@ SMODS.DeckSkin {
 	has_ds_card_ui = has_ds_card_ui_base,
 	generate_ds_card_ui = generate_ds_card_ui_base,
 }
+
+-- Suit Based Definitions
+-- Note: King, Queen, Jack and Ace cards only
+
+for i, suit in ipairs(suits) do
+	SMODS.DeckSkin {
+		key = atlas_base .. "_deck_" .. suit:lower() .. "_skin",
+		suit = suit,
+		loc_txt = descriptions[4],
+		palettes = {
+			{
+				key = "lc",
+				ranks = ranks,
+				display_ranks = {"King", "Queen", "Jack", "Ace"},
+				atlas = atlas_suit_lc.key,
+				pos_style = 'deck',
+			},
+			{
+				key = "hc",
+				ranks = ranks,
+				display_ranks = {"King", "Queen", "Jack", "Ace"},
+				atlas = atlas_suit_hc.key,
+				pos_style = 'deck',
+			},
+		},
+		has_ds_card_ui = has_ds_card_ui_base,
+		generate_ds_card_ui = generate_ds_card_ui_base,
+	}
+end
+
+-- Full Suit Based Definitions
+-- Note: All cards included
+
+for i, suit in ipairs(suits) do
+	SMODS.DeckSkin {
+		key = atlas_base .. "_deck_full_" .. suit:lower() .. "_skin",
+		suit = suit,
+		loc_txt = descriptions[5],
+		palettes = {
+			{
+				key = "lc",
+				ranks = ranks,
+				display_ranks = {"King", "Queen", "Jack", "Ace"},
+				atlas = atlas_full_lc.key,
+				pos_style = 'deck',
+			},
+			{
+				key = "hc",
+				ranks = ranks,
+				display_ranks = {"King", "Queen", "Jack", "Ace"},
+				atlas = atlas_full_hc.key,
+				pos_style = 'deck',
+			},
+		},
+		has_ds_card_ui = has_ds_card_ui_base,
+		generate_ds_card_ui = generate_ds_card_ui_base,
+	}
+end
